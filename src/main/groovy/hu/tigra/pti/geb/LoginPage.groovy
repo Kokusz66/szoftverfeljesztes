9ab7b46385bd4ac4e7719389d281bb2e517ef3f7
Tcsaba66 <38902288+Kokusz66@users.noreply.github.com>
2020-04-02 14:58:48 +0200

    hazi feladat

--- src/main/groovy/hu/tigra/pti/geb/LoginPage.groovy
+++ src/main/groovy/hu/tigra/pti/geb/LoginPage.groovy
@@ -13,5 +13,8 @@ class LoginPage extends Page {
         emailAddress { $('form[id="login_form"] input[id="email"]') }
         password { $('form[id="login_form"] input[id="passwd"]') }
         loginButton { $('form[id="login_form"] button[id="SubmitLogin"]') }
+        createForm{ $('form[id="create-account_form"]') }
+        createAccButton { $('form[id="create-account_form"] button[id="SubmitCreate"]')}
+        newEmailAddress { $('form[id="create-account_form"] input[id="email_create"]') }
     }
 }
